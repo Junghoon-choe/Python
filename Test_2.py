538275173681867ce4d38070f91133778a41b78e
Joe <hjc217@naver.com>
2020-11-06 09:15:21 +0900

    first Week

--- Test_2.py
+++ Test_2.py
@@ -1,30 +1,30 @@
 # << 파이썬에서 주석처리.
-#변수 정의
-a='Joe'
-b='Hello'
-c=1580
-d=12.43
-e=30
+# 변수 정의
+a = 'Joe'
+b = 'Hello'
+c = 1580
+d = 12.43
+e = 30
 
-#불린선언
+# 불린선언
 falseTest = False
 trueTest = True
 
-#변수 출력
-print('a=',a)
-print('b=',b)
-print('c=',c)
-print('d=',d)
-print('e=',e)
-print('c+e=',c+e)
+# 변수 출력
+print('a=', a)
+print('b=', b)
+print('c=', c)
+print('d=', d)
+print('e=', e)
+print('c+e=', c + e)
 
-#파라미터 : 출력 또는 입력할 내용을 적어서 함수화 시키는 장소 ,https://lancoding.tistory.com/ << 참고하기
+# 파라미터 : 출력 또는 입력할 내용을 적어서 함수화 시키는 장소 ,https://lancoding.tistory.com/ << 참고하기
 
-A=3
-B='qwer'
-C="abc"
-D=3.14
-E=True
+A = 3
+B = 'qwer'
+C = "abc"
+D = 3.14
+E = True
 
 '''
 print('A=',A,'type=',type(A))
@@ -32,34 +32,32 @@ print('B=',B,'type=',type(B))
 print('C=',C,'type=',type(C))
 print('D=',D,'type=',type(D))
 '''
-#위와같이 ''' 로 묶어주면 전체 주석처리 가능 
-print('E=',E,'type=',type(E))
+# 위와같이 ''' 로 묶어주면 전체 주석처리 가능
+print('E=', E, 'type=', type(E))
 
-#파이썬은 모두 객체타입이다 (강사님 말씀)
+# 파이썬은 모두 객체타입이다 (강사님 말씀)
 
-aa=10
-bb=20
+aa = 10
+bb = 20
 
-cc=aa+bb
-print(aa,'+',bb,'=',cc)
+cc = aa + bb
+print(aa, '+', bb, '=', cc)
 
-ww = str(aa)+str(bb) # 형변환 하는 방법
-print(ww) # 출력결과 1020
+ww = str(aa) + str(bb)  # 형변환 하는 방법
+print(ww)  # 출력결과 1020
 
 name = input('이름을 입력하시오')
 
-
-if(name=="Joe"):
+if (name == "Joe"):
     print('안녕')
 
 while True:
-    str= input('메세지를 입력하세요. 멈추려면 /stop을 입력')
+    str = input('메세지를 입력하세요. 멈추려면 /stop을 입력')
     if str == '/stop':
-    break; #왜 오류가 나는지 알아보기.
-print('루프종료')
-else:
-    print(str)
-
+        break;  # 왜 오류가 나는지 알아보기.
+        print('루프종료')
+    else:
+        print(str)
 
 '''
 while True:
@@ -79,7 +77,6 @@ else:
     print('다시 입력해주세요.')
     '''
 
-
 '''
 age = int (input('나이를 입력하시오'))
 dept = input('학과를 입력하시오')
@@ -91,6 +88,3 @@ print('age:',age)
 print('age about 5years later:',c)
 print('dept:',dept)
 '''
-
-
-
